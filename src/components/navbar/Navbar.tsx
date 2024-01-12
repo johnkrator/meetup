c86022d3b84bf9c8078a36a01573d446d1ce7456
johncreator <chukwuchidieberejohn@gmail.com>
2024-01-12 23:02:33 +0100

    initial commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,24 +3,26 @@ import brandLogo from "../../assets/brand-logo.svg";
 
 const Navbar = () => {
     return (
-        <Container maxWidth="container.xl">
-            <header>
-                <Box display="flex" alignItems="center" justifyContent="space-between" py={2}>
-                    <Box>
-                        <Image src={brandLogo} alt="logo"/>
-                    </Box>
+        <Box position="sticky" top={0} zIndex={1} background="white">
+            <Container maxWidth="container.xl">
+                <header>
+                    <Box display="flex" alignItems="center" justifyContent="space-between" py={2}>
+                        <Box>
+                            <Image src={brandLogo} alt="logo"/>
+                        </Box>
 
-                    <Box>
-                        <Button pr={3} color="gray.600" fontSize="sm" variant="link">
-                            Login
-                        </Button>
-                        <Button color="gray.600" fontSize="sm" variant="link">
-                            Signup
-                        </Button>
+                        <Box>
+                            <Button pr={3} color="gray.600" fontSize="sm" variant="link">
+                                Login
+                            </Button>
+                            <Button color="gray.600" fontSize="sm" variant="link">
+                                Signup
+                            </Button>
+                        </Box>
                     </Box>
-                </Box>
-            </header>
-        </Container>
+                </header>
+            </Container>
+        </Box>
     );
 };
 
