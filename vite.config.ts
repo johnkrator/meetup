792e84ff30a3d1edba8e8296aa5e470237b7f10f
johncreator <chukwuchidieberejohn@gmail.com>
2024-01-16 03:06:04 +0100

    favicon fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import favicon from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        process.env.NODE_ENV === "production" && favicon({
-            logo: "./public/apple-touch-icon.png"
-        })
     ],
 });
